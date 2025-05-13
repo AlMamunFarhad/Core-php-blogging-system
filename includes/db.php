d8d6fd649dbe99783ac878631c7bb18575dcebdb
Farhad <122156729+AlMamunFarhad@users.noreply.github.com>
2025-05-13 13:15:56 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php ob_start();
 $db['db_host'] = "localhost";
 $db['db_user'] = "farhadwe_farhad1";
-$db['db_pass'] = "|farhad|mikky|764476";
+$db['db_pass'] = "";
 $db['db_name'] = "farhadwe_php_cms_2";
 
 foreach ($db as $key => $value) {
